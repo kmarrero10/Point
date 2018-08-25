00d859ac8cc9f4c0b8614403618018101ce68d3a
Kyle Marrero <kylemarrero@Kyles-MacBook-Pro.local>
2018-08-25 16:29:22 -0500

    init

---
+++ src/com/kylemarrero/BruteCollinearPoints.java
@@ -0,0 +1,18 @@
+package com.kylemarrero;
+
+public class BruteCollinearPoints {
+
+    LineSegment[] lineSegments =  new LineSegment[4];
+
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    public LineSegment[] segments() {
+        return lineSegments;
+    }
+}
